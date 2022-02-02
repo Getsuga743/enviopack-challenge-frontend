fdf4f8cdf23866d69139b82da88ad9f5a6998b88
ivanmaierg <ivan_maier@hotmail.com>
2022-02-01 22:15:57 -0300

    feat(theme): add theme application and Theme component

---
+++ src/styles/themes/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  fonts: { main: '"Roboto", "sans-serif"' },
+  colors: {
+    primary: '#212221',
+    accent: '#EAECEF',
+    background: '#FFFFFF',
+    black: '#000000',
+  },
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)',
+  },
+};
+
+export default theme;
