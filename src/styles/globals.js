c104d7744491fc4351a7f1dfba11bc5d1f5dca3e
ivanmaierg <ivan_maier@hotmail.com>
2022-02-01 22:16:19 -0300

    feat(theme): add global styles

---
+++ src/styles/globals.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+     *, *::after, *::before {
+    box-sizing: border-box;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    };
+     *{
+        box-sizing: border-box;
+        margin:0;
+    };
+    html{
+        font-size: 62.5%;
+        scroll-behavior: smooth;
+    };
+    body{
+        font-family:${(props) => props.theme.fonts.main};
+        font-size: 1.6rem;
+        background-color:${(props) => props.theme.colors.background};
+        color: ${(props) => props.theme.colors.black};
+        cursor:default;
+    };
+    h1,h2,h3,h4,h5,h6,button {
+        font-family: ${(props) => props.theme.fonts.main};
+    }
+    a {
+        text-decoration: none;
+    };
+    li{
+        list-style: none;
+    };
+
+`;
+export default GlobalStyles;
