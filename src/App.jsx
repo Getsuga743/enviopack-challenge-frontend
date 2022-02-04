af77397541ad4c680c9b4dbef145a0995f5516bb
ivanmaierg <ivan_maier@hotmail.com>
2022-02-04 09:26:02 -0300

    feat(app): add Spinner screen and loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,37 @@
 /* eslint-disable import/no-named-as-default-member */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { UserContext } from './context/UserContext';
+import Container from './components/ui/containers/Container';
+import PageContainer from './components/ui/containers/PageContainer';
+import Spinner from './components/ui/Spinner';
+import useUser from './hooks/useUser';
 import Cart from './pages/Cart';
 import Catalog from './pages/Catalog';
 import Error from './pages/Error/Index';
 import Layout from './pages/Layout';
-import Succes from './pages/Succes';
+import Success from './pages/Success';
 
 function App() {
-  const { loading: isLoading } = React.useContext(UserContext);
+  const { loading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <PageContainer>
+        <Container
+          style={{ margin: 'auto', justifyContent: 'center', height: '90vh', alignItems: 'center' }}
+        >
+          <Spinner />
+        </Container>
+      </PageContainer>
+    );
 
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route index path="/" element={<Catalog />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Catalog />} />
           <Route path="cart" element={<Cart />} />
-          <Route path="cart/success" element={<Succes />} />
+          <Route path="cart/success" element={<Success />} />
           <Route path="cart/error" element={<Error />} />
           <Route
             path="*"
@@ -31,8 +43,8 @@ function App() {
               </main>
             }
           />
-        </Routes>
-      </Layout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
